93ee9af34f5da4ae7a3a0538c68325e207ae4982
TejasNeswi <tejasneswi2709@gmail.com>
2025-08-13 23:23:34 +0530

    connected postgreSQL using springdatajpa

---
+++ src/main/java/com/myprojects/ems_backend/repository/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package com.myprojects.ems_backend.repository;
+
+import com.myprojects.ems_backend.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+
+}
